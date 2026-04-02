6648143eac38f308876c1010ca74f980b5d9b94d
Vidhi Pandit <Vidhi.pandit@contoso.com>
2026-04-02 18:01:16 +0530

    fixed db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,19 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    dialectOptions: {
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
-    logging: false,
-  }
-);
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected ✅"))
+  .catch((err) => console.log("Error:", err));
 
 module.exports = sequelize;
